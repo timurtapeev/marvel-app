b6b9f2fb49a1a25b2e6ab3c264e282b231a99238
tapeevtimur <tapeevtimur@mail.ru>
2022-12-21 15:31:04 +0500

    services: added api call

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,6 @@
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=2d08d0f9265171c9529ece70f4979790'
     getResource = async (url) => {
         let res = await fetch(url);
     
@@ -8,4 +10,14 @@ class MarvelService {
     
         return await res.json();
     }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=220&${this._apiKey}`);
+    }
+
+    getCharacters = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+    }
 }
+
+export default MarvelService;
