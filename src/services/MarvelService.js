fa4c7a6b5098ca9177726d5189948d29931e52b0
tapeevtimur <tapeevtimur@mail.ru>
2022-12-23 17:22:06 +0500

    char-list: added additional character loading

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=2d08d0f9265171c9529ece70f4979790'
+    _apiKey = 'apikey=2d08d0f9265171c9529ece70f4979790';
+    _baseOffset = 210;
+
     getResource = async (url) => {
         let res = await fetch(url);
     
@@ -11,8 +13,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=220&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
